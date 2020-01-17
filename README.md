08861f6e5ba5928eee03c7baa07807872b1efdba
Tuscany <lizlevine.rigie@gmail.com>
2020-01-17 18:15:03 -0500

    feat: Add spotify functions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # liri-node-app
 This Node.js app, LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies using these technologies; Node-Spotify-API, Axios, Bands In Town API, Moment, DotEnv
+
+
+# Technologies used in this app: 
+node.js
+Axios
+Spotify API
+Bands in Town API
+OMDB API
--- liri.js
+++ liri.js
@@ -1,3 +1,135 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require("fs");
+var request = require("request");
+var moment = require("moment");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+//var defaultMovie = "The Hangover";
+var action = process.argv[2];
+var value = process.argv[3];
 
+function pick(command, query) {
+  // create switch statements to hold different arguments from user
+  switch (command) {
+    case "concert-this":
+      getBands(query);
+      break;
+    case "spotify-this-song":
+      getSongs(query);
+      break;
+    case "movie-this":
+      getMovies(query);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      break;
+  }
+}
+function getBands(artist) {
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      console.log("Name of the venue:", response.data[0].venue.name);
+      console.log("Venue location:", response.data[0].venue.city);
+      var eventDate = moment(response.data[0].datetime).format("MM/DD/YYYY");
+      console.log("Date of the Event:", eventDate);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 var spotify = new Spotify(keys.spotify);
+function getSongs(songName) {
+  if (songName === "") {
+    songName = "Payphone";
+  }
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+      var songs = data.tracks.items;
+      for (var i = 0; i < songs.length; i++) {
+      console.log(i);
+      console.log("Artist(s): ",songs[i].album.artists[0].name);
+      console.log("Preview Song: ",songs[i].preview_url);
+      console.log("Album: ",songs[i].album.name);
+      }
+    }
+  );
+}
+// copied and pasted below from NPM API-documentation - LL
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+
+// spotify.search({ type: "track", query: "All the Small Things" }, function(
+//   err,
+//   data
+// ) {
+//   if (err) {
+//     return console.log("Error occurred: " + err);
+//   }
+
+//   console.log(data);
+// });
+function getMovies(movieName) {
+  axios
+    .get("http://www.omdbapi.com/?apikey=trilogy&t=" + movieName)
+    .then(function(data) {
+      var results = `
+        Title of the movie: ${data.data.Title}
+        Year the movie came out: ${data.data.Year}
+        IMDB Rating of the movie: ${data.data.Rated}
+        Rotten Tomatoes Rating of the movie: ${data.data.Ratings[1].Value}
+        Country where the movie was produced: ${data.data.Country}
+        Language of the movie: ${data.data.Language}
+        Plot of the movie: ${data.data.Plot}
+        Actors in the movie: ${data.data.Actors}`;
+      console.log(results);
+    });
+}
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    data = data.split(",");
+    var action = data[0];
+    var value = data[1];
+    // getSongs(value)
+    switch (action) {
+      case "concert-this":
+        getBands(value);
+        break;
+      case "spotify-this-song":
+        getSongs(value);
+        break;
+      case "movie-this":
+        getMovies(value);
+        break;
+      default:
+        break;
+    }
+  });
+
+  var runThis = function(argOne, argTwo) {
+    PublicKeyCredential(argOne, argTwo);
+  };
+}
+//Function which takes in command line arguments and executes correct function accordingly
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv.slice(3).join(" "));
